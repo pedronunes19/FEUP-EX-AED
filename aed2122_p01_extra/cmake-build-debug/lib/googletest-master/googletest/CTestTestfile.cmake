552de5e925ba19d505cde84fe9dabd4cc1627b35
pedronunes19 <pedromiguelmnunes@gmail.com>
2021-11-01 19:31:32 +0000

    working .gitignore

--- aed2122_p01_extra/cmake-build-debug/lib/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/pedro/Documents/Pedro/Faculdade/LEIC/AED/Praticas/aed2122_p01_extra/lib/googletest-master/googletest
-# Build directory: C:/Users/pedro/Documents/Pedro/Faculdade/LEIC/AED/Praticas/aed2122_p01_extra/cmake-build-debug/lib/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
