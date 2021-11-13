ea166d521897bb5608e6a394710ce39fb42c7a5d
pedronunes19 <pedromiguelmnunes@gmail.com>
2021-11-13 00:40:33 +0000

    improve efficiency for ex4

--- Mooshak/vol01_num.cpp
+++ Mooshak/vol01_num.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
+
+long long minNecessary(int sum){
+    int i = 0, min;
+    do{
+        i++;
+        min = 9;
+        min  *=  i;
+    }while(min < sum);
+    int j = 0;
+    min = (i-1) * 9;
+    while (min < sum){
+        min = (i-1) * 9;
+        j++;
+        min += j;
+    }
+    return (j* pow(10, i)) + (pow(10, i-1)-1);
+}
 
 int main(){
     int t;
-    std::vector <int> v;
+    std::vector <long long> v;
     std::cin >> t;
     for (int i=1;i<=t;i++){
-        int iNum0, sum, temp;
+        long long iNum0, sum, temp;
         std::cin >> iNum0 >> sum;
+        iNum0 = std::max(minNecessary(sum), iNum0)  - 1;
         while (temp!=sum){
             temp=0;
             iNum0++;
-            int iNum = iNum0;
+            long long iNum = iNum0;
             while(iNum>=10){
                 temp += iNum%10;
                 iNum -=iNum%10;
@@ -23,7 +42,7 @@ int main(){
         temp = 0;
 
     }
-    for (int i: v){
+    for (long long i: v){
         std::cout << i << std::endl;
     }
     return 0;
